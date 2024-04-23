e475133ff54dbbb3f68c094b3fea0831b44b1da6
SkyBLizzard0 <165757578+SkyBLizzard0@users.noreply.github.com>
2024-04-23 09:46:26 +0530

    Intial Commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+function Home(props) {
+
+    var uname = props.uname;
+    return <h1>This is Home Page {uname}</h1>
+}
+function House() {
+    return <h1>This is House Page</h1>
+}
+
+export{Home, House}
